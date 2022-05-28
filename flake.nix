1f1f8c034867a3742918a34c8c213cfcfde57933
Matthew Toohey <contact@mtoohey.com>
2022-05-28 10:07:45 -0400

    build: nixify

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "spotify-playlist-randomizer";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = { self, nixpkgs, utils, poetry2nix }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ poetry2nix.overlay ];
+        }; in
+        with pkgs; rec {
+          packages.default = pkgs.poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+          };
+
+          devShells.default = (pkgs.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+          }).overrideAttrs (oldAttrs: {
+            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+              python3Packages.python-lsp-server
+            ];
+          });
+        }) // {
+      overlays.default = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: _: { spotify-playlist-randomizer = self.packages."${final.system}".default; })
+      ];
+    };
+}
